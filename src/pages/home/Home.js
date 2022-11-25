ad0be0614db5d030884719101ef25358c409853e
Enes Kaya <eneskaya5261@gmail.com>
2022-11-25 18:08:54 +0300

    feat(components): create topbar component

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,8 @@
 import React from 'react'
+import Topbar from '../../components/topbar/Topbar'
 
 export default function Home() {
   return (
-    <div>
-        this is home
-    </div>
+    <Topbar/>
   )
 }
