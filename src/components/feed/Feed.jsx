bcfb687eba4fe480cb6ec4c7973bb1f20053add5
Enes Kaya <eneskaya5261@gmail.com>
2022-11-27 13:07:10 +0300

    feat(components): create share component and add it to feed

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,9 +1,12 @@
+import Share from "../share/Share"
 import "./feed.css"
 
 export default function Feed() {
   return (
     <div className="feed">
-      feed
+      <div className="feedWrapper">
+        <Share />
+      </div>
     </div>
   )
 }
