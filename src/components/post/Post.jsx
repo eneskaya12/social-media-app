90d7e90fedd5e1feea2b0938b167c93a6939c347
Enes Kaya <eneskaya5261@gmail.com>
2022-11-27 19:33:51 +0300

    feat(post): add like functionality with useState hook

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,8 +1,17 @@
 import "./post.css"
 import { MoreVert } from "@mui/icons-material"
 import { Users } from "../../dummyData"
+import { useState } from "react";
 
 export default function Post({post}) {
+  const [like, setLike] = useState(post.like);
+  const [isLiked, setIsLiked] = useState(false);
+  
+  const likeHandler =()=>{
+    setLike(isLiked ? like - 1 : like + 1)
+    setIsLiked(!isLiked)
+  }
+  
   return (
     <div>
       <div className="post">
@@ -23,9 +32,9 @@ export default function Post({post}) {
             </div>
             <div className="postBottom">
                 <div className="postBottomLeft">
-                    <img className="likeIcon" src="/assets/heart.png" alt=""/>
-                    <img className="likeIcon" src="/assets/like.png" alt=""/>
-                    <span className="postlikeCounter">{post.like} people like it</span>
+                    <img className="likeIcon" src="/assets/like.png" onClick={likeHandler} alt=""/>
+                    <img className="likeIcon" src="/assets/heart.png" onClick={likeHandler} alt=""/>
+                    <span className="postlikeCounter">{like} people like it</span>
                 </div>
                 <div className="postBottomRight">
                     <span className="postCommentText">{post.comment} comments</span>
