7a81595f5e1b3f93cc09c9f3e18c2cabd7212e95
Enes Kaya <eneskaya5261@gmail.com>
2022-11-29 16:30:54 +0300

    feat(pages): create profile page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Home from "./pages/home/Home";
+import Profile from "./pages/profile/Profile";
 
 function App() {
   return (
     <div>
-      <Home/>
+      <Profile/>
     </div>
   );
 }
---
+++ src/pages/profile/Profile.jsx
@@ -0,0 +1,32 @@
+import "./profile.css"
+import Topbar from '../../components/topbar/Topbar'
+import Sidebar from '../../components/sidebar/Sidebar'
+import Feed from '../../components/feed/Feed'
+import Rightbar from '../../components/rightbar/Rightbar'
+
+export default function Profile() {
+    return (
+        <>
+            <Topbar />
+            <div className='profile'>
+                <Sidebar />
+                <div className="profileRight">
+                    <div className="profileRightTop">
+                        <div className="profileCover">
+                            <img className="profileCoverImg" src="/assets/post/3.jpeg" alt="" />
+                            <img className="profileUserImg" src="/assets/person/7.jpeg" alt="" />
+                        </div>
+                    </div>
+                    <div className="profileInfo">
+                        <h4 className="profileInfoName">Enes Kaya</h4>
+                        <span className="profileInfoDesc">Hello people!</span>
+                    </div>
+                    <div className="profileRightBottom">
+                        <Feed />
+                        <Rightbar profile/>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
