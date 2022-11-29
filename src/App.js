ee984ef7767b60e8bb18c5fb07d18a6da176edb0
Enes Kaya <eneskaya5261@gmail.com>
2022-11-29 18:36:36 +0300

    fix(rightbar): add ternary operator to manage both profile and home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import Home from "./pages/home/Home";
-import Login from "./pages/login/Login";
-import Profile from "./pages/profile/Profile";
-import Register from "./pages/register/Register";
 
 function App() {
   return (
     <div>
-      <Register/>
+      <Home/>
     </div>
   );
 }
--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -74,7 +74,7 @@ export default function Rightbar({ profile }) {
   return (
     <div className="rightbar">
       <div className="rightbarWrapper">
-        <ProfileRightbar />
+        {profile ? <ProfileRightbar /> : <HomeRightbar />}
       </div>
     </div>
   )
