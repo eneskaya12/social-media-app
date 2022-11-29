88c8afc2e3e21f793cf458c89659cb6806b063a3
Enes Kaya <eneskaya5261@gmail.com>
2022-11-29 18:05:06 +0300

    feat(pages): create login page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
 import Profile from "./pages/profile/Profile";
 
 function App() {
   return (
     <div>
-      <Profile/>
+      <Login/>
     </div>
   );
 }
