1d6fcce64ed446c6040a5ddf3b8725f4aa4a72e7
Enes Kaya <eneskaya5261@gmail.com>
2022-12-13 15:29:28 +0300

    feat: add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import Profile from "./pages/profile/Profile";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <div>
-      <Home/>
-    </div>
-  );
-}
+    return (
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/profile/:username" element={<Profile />} />
+        </Routes>
+      </Router>
+    );
+  }
 
 export default App;
