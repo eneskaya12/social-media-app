60704e9c5b45fefe37b33044e1ba5fb512a822d6
Enes Kaya <eneskaya5261@gmail.com>
2022-12-20 19:45:05 +0300

    feat: update login page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,21 +1,31 @@
+import { useRef } from "react";
 import "./login.css"
 
 export default function Login() {
+
+  const email = useRef();
+  const password = useRef();
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    console.log(email.current.value);
+  }
+
   return (
     <div className="login">
       <div className="loginWrapper">
         <div className="loginLeft">
-            <h3 className="loginLogo">socialspace</h3>
-            <span className="loginDesc">Connect with friends and the world around you on socialspace.</span>
+          <h3 className="loginLogo">socialspace</h3>
+          <span className="loginDesc">Connect with friends and the world around you on socialspace.</span>
         </div>
         <div className="loginRight">
-            <div className="loginBox">
-                <input placeholder="Email" className="loginInput"/>
-                <input placeholder="Password" className="loginInput"/>
-                <button className="loginButton">Log In</button>
-                <span className="loginForgot">Forgot Password?</span>
-                <button className="loginRegisterButton">Create a New Account</button>
-            </div>
+          <form className="loginBox" onSubmit={handleClick}>
+            <input placeholder="Email" type="email" required className="loginInput" ref={email}/>
+            <input placeholder="Password" type="password" required minLength="6" className="loginInput" ref={password}/>
+            <button className="loginButton">Log In</button>
+            <span className="loginForgot">Forgot Password?</span>
+            <button className="loginRegisterButton">Create a New Account</button>
+          </form>
         </div>
       </div>
     </div>
