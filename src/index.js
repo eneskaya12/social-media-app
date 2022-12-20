aa40c5083bf6c3fc2c6ae7820e5f7c18bd6783b8
Enes Kaya <eneskaya5261@gmail.com>
2022-12-20 20:30:47 +0300

    feat: create AuthContext.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import {AuthContextProvider} from "./context/AuthContext";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthContextProvider>
+      <App />
+    </AuthContextProvider>
   </React.StrictMode>
 );
