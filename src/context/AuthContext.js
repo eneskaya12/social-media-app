d625b175a62474c52877699d914f79ab56e61015
Enes Kaya <eneskaya5261@gmail.com>
2022-12-20 20:50:43 +0300

    feat: create AuthReducer.js & AuthActions.js

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,4 +1,5 @@
 import { createContext, useReducer } from "react";
+import AuthReducer from "./AuthReducer";
 
 const INITIAL_STATE = {
     user: null,
@@ -9,7 +10,7 @@ const INITIAL_STATE = {
 export const AuthContext = createContext(INITIAL_STATE);
 
 export const AuthContextProvider = ({children}) => {
-    const [state, dispatch] = useReducer(AuthContext, INITIAL_STATE);
+    const [state, dispatch] = useReducer(AuthReducer, INITIAL_STATE);
 
     return(
         <AuthContext.Provider value={{user:state.user, isFetching:state.isFetching, error:state.error, dispatch}}>
