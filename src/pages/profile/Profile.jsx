0dbe0f273c952d3301609a9f0c12990a902e6753
Enes Kaya <eneskaya5261@gmail.com>
2022-12-20 18:42:18 +0300

    feat: make content dynamic with useParams

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -5,18 +5,20 @@ import Feed from '../../components/feed/Feed'
 import Rightbar from '../../components/rightbar/Rightbar'
 import { useEffect, useState } from "react"
 import axios from "axios"
+import { useParams } from "react-router";
 
 export default function Profile() {
     const PF = process.env.REACT_APP_PUBLIC_FOLDER;
     const [user, setUser] = useState({});
+    const username = useParams().username;
 
     useEffect(() => {
         const fetchUser = async () => {
-          const res = await axios.get(`/users?username=hey`);
+          const res = await axios.get(`/users?username=${username}`);
           setUser(res.data);
         }
         fetchUser();
-      }, []);
+      }, [username]);
 
     return (
         <>
@@ -35,7 +37,7 @@ export default function Profile() {
                         <span className="profileInfoDesc">{user.desc}</span>
                     </div>
                     <div className="profileRightBottom">
-                        <Feed username="hey"/>
+                        <Feed username={username}/>
                         <Rightbar user={user}/>
                     </div>
                 </div>
