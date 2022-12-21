2b60db78b2de133b27a4f41a67b88a8ad95601d6
Enes Kaya <eneskaya5261@gmail.com>
2022-12-21 11:01:33 +0300

    feat: reform login system with context API

--- src/App.js
+++ src/App.js
@@ -2,19 +2,24 @@ import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 import Profile from "./pages/profile/Profile";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
 
 function App() {
-    return (
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/profile/:username" element={<Profile />} />
-        </Routes>
-      </Router>
-    );
-  }
+
+  const { user } = useContext(AuthContext);
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={user ? <Home /> : <Register />} />
+        <Route path="/login" element={user ? <Navigate replace to="/" /> : <Login />} />
+        <Route path="/register" element={user ? <Navigate replace to="/" /> : <Register />} />
+        <Route path="/profile/:username" element={<Profile />} />
+      </Routes>
+    </Router>
+  );
+}
 
 export default App;
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,15 +1,21 @@
-import { useRef } from "react";
-import "./login.css"
+import { useContext, useRef } from "react";
+import "./login.css";
+import { loginCall } from "../../apiCalls";
+import { AuthContext } from "../../context/AuthContext";
+import { CircularProgress } from "@mui/material"
 
 export default function Login() {
 
   const email = useRef();
   const password = useRef();
+  const { user, isFetching, error, dispatch } = useContext(AuthContext);
 
   const handleClick = (e) => {
     e.preventDefault();
-    console.log(email.current.value);
+    loginCall({ email: email.current.value, password: password.current.value }, dispatch);
   }
+
+  console.log(user);
 
   return (
     <div className="login">
@@ -20,11 +26,11 @@ export default function Login() {
         </div>
         <div className="loginRight">
           <form className="loginBox" onSubmit={handleClick}>
-            <input placeholder="Email" type="email" required className="loginInput" ref={email}/>
-            <input placeholder="Password" type="password" required minLength="6" className="loginInput" ref={password}/>
-            <button className="loginButton">Log In</button>
+            <input placeholder="Email" type="email" required className="loginInput" ref={email} />
+            <input placeholder="Password" type="password" required minLength="6" className="loginInput" ref={password} />
+            <button className="loginButton" disabled={isFetching}>{isFetching ? <CircularProgress style={{ color: "white" }} /> : "Log In"}</button>
             <span className="loginForgot">Forgot Password?</span>
-            <button className="loginRegisterButton">Create a New Account</button>
+            <button className="loginRegisterButton">{isFetching ? <CircularProgress style={{ color: "white" }} /> : "Create a New Account"}</button>
           </form>
         </div>
       </div>
