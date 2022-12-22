c21ae1b5ebe4b25db391f20ea89e572a082d834f
Enes Kaya <eneskaya5261@gmail.com>
2022-12-22 14:31:24 +0300

    feat: like and dislike functionality with context API

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,15 +1,21 @@
 import "./post.css"
 import { MoreVert } from "@mui/icons-material"
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import axios from "axios";
 import { format } from "timeago.js";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
 
 export default function Post({ post }) {
   const [like, setLike] = useState(post.likes.length);
   const [isLiked, setIsLiked] = useState(false);
   const [user, setUser] = useState({});
   const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+  const { user: currentUser } = useContext(AuthContext);
+
+  useEffect(()=>{
+    setIsLiked(post.likes.includes(currentUser._id));
+  },[currentUser._id, post.likes]);
 
   useEffect(() => {
     const fetchUser = async () => {
@@ -20,6 +26,12 @@ export default function Post({ post }) {
   }, [post.userId]);
 
   const likeHandler = () => {
+    try {
+      axios.put("/posts/" + post._id + "/like", { userId: currentUser._id });
+    } catch (err) {
+
+    }
+
     setLike(isLiked ? like - 1 : like + 1)
     setIsLiked(!isLiked)
   }
