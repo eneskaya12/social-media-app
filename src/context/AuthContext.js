37291c2bafb033b2156dea8f8fbf458fe6f95d8f
Enes Kaya <eneskaya5261@gmail.com>
2023-01-30 11:38:15 +0300

    feat(AuthContext.js): add localStorage

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,8 +1,8 @@
-import { createContext, useReducer } from "react";
+import { createContext, useReducer, useEffect } from "react";
 import AuthReducer from "./AuthReducer";
 
 const INITIAL_STATE = {
-    user: null,
+    user: JSON.parse(localStorage.getItem("user")) || null,
     isFetching: false,
     error: false
 };
@@ -11,6 +11,10 @@ export const AuthContext = createContext(INITIAL_STATE);
 
 export const AuthContextProvider = ({children}) => {
     const [state, dispatch] = useReducer(AuthReducer, INITIAL_STATE);
+
+    useEffect(()=> {
+        localStorage.setItem("user", JSON.stringify(state.user))
+      },[state.user])
 
     return(
         <AuthContext.Provider value={{user:state.user, isFetching:state.isFetching, error:state.error, dispatch}}>
