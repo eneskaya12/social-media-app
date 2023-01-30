797932fb36ca478be1c27929f43864f59f85a397
Enes Kaya <eneskaya5261@gmail.com>
2023-01-30 11:58:03 +0300

    fix(Feed.jsx): fix visibility of share component

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -23,7 +23,7 @@ export default function Feed({ username }) {
   return (
     <div className="feed">
       <div className="feedWrapper">
-        {username === user?.username && <Share />}
+        {(!username || username === user?.username) && <Share />}
         {posts.map(p => (
           <Post key={p._id} post={p} />
         ))}
